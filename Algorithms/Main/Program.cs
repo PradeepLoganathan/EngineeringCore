80ef3c9b75822b4d8f7efe418f7f3d2560bf482b
Pradeep Loganathan <Pradeep Loganathan>
2016-11-08 17:20:17 +0530

    BubbleSort sorting algorithm

---
+++ Algorithms/Main/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using EngineeringCore.Algorithms;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //List<int> numbers = new List<int> {1, 2, 3, 6, 7, 9};
+            List<int> numbers = new List<int> {9, 7, 6, 3, 2, 1};
+            
+            numbers.BubblesortAscending(Comparer<int>.Default);
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
