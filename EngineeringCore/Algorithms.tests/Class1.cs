605fefbf9ac0a5da1bb8dc910c725e92990c712a
Pradeep <pradeep.loganathan@hotmail.com>
2016-11-15 12:37:33 +0530

    Still very minor changes

--- EngineeringCore/Algorithms.tests/Class1.cs
+++ EngineeringCore/Algorithms.tests/Class1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Xunit;
+ 
 
 namespace DataStructures.tests
 {
--- EngineeringCore/Algorithms/Sorting/BubbleSort.cs
+++ EngineeringCore/Algorithms/Sorting/BubbleSort.cs
@@ -6,8 +6,28 @@ using System.Threading.Tasks;
 
 namespace Algorithms.Sorting
 {
-    class BubbleSort
+    public static class BubbleSorter
     {
+        public static void BubbleSort<T>(this IList<T> collection, Comparer<T> comparer)
+        {
+            comparer = comparer ?? Comparer<T>.Default;
+                
+            for (int i = 0; i < collection.Count; i++)
+            {
+                for(int j = 0; j < collection.Count - 1; j++)
+                {
+                    if (comparer.Compare(collection[j], collection[j + 1]) > 0)
+                    {
+                        T temp = collection[j + 1];
+                        collection[j + 1] = collection[j];
+                        collection[j] = temp;
+                    }
+                }
+            }
+        }
+
+
+        
 
     }
 }
