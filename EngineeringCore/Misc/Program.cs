4dfbb2853639c2406437b138a07b28b6eda4b60f
Pradeep Loganathan <Pradeep Loganathan>
2016-12-01 19:10:02 +0530

    optimization for char length

--- EngineeringCore/Misc/Program.cs
+++ EngineeringCore/Misc/Program.cs
@@ -5,7 +5,12 @@ namespace ConsoleApplication
     {
         public static void Main(string[] args)
         {
-            DuplicateChars(args[0].ToLower());
+            //DuplicateChars(args[0].ToLower());
+            if( IsPermutation("GOD", "dOG") )
+                System.Console.WriteLine("It is a permutation");
+            else
+                System.Console.WriteLine("It is not a permutation");
+
         }
 
         public static bool DuplicateChars(string str)
@@ -13,7 +18,9 @@ namespace ConsoleApplication
             bool[] charfound = new bool[26];
             int index = 0;
             
-            
+            if(str.Length > charfound.Length) 
+                return true;
+
             for(int i = 0; i < str.Length ; ++i)
             {
                 index = (byte)(str[i]);
@@ -25,6 +32,22 @@ namespace ConsoleApplication
             }  
 
             return false;
+        }
+
+        public static bool IsPermutation(string first, string second)
+        {
+            if(first.Length != second.Length)
+                return false;
+            
+            int firstcount = 0, secondcount = 0;
+            //add the byte values of the two strings and compare. If the byte sum is equal they are permutations
+            for(int i = 0; i < first.Length; i++)
+            {
+                firstcount += (byte)first[i];
+                secondcount += (byte)second[i];
+            }
+
+            return firstcount == secondcount;
         }
     }
 }
